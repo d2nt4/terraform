a8a7ce84ca310caa35e2e807386bd352a6c8fd11
d2nt4 <dtdtorres545@gmail.com>
2024-12-12 19:51:35 +0100

    Versión final de la práctica de Terraform (EC2 y RDS)

--- modules/rds/rds.tf
+++ modules/rds/rds.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "mi_rds" {
   password             = var.db_password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
-  publicly_accessible  = false
+  publicly_accessible  = true
 
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
